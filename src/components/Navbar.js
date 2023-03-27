0a39de0a1fddb163d6e5e85a05e4259ccb37390d
thuotracy <tracyjacobs775@gmail.com>
2023-03-27 15:35:28 +0300

    added styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,26 @@ import { MenuData } from '../data/MenuData';
 const Nav = styled.nav`
     height: 60px;
     display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
     background: #000;
+`;
+
+const NavLink = css`
+   color: #fff;
+   display:flex;
+   align-items: center;
+   padding: 0 1rem;
+   height: 100%;
+   cursor: pointer;
 `;
 
 const Logo = styled(Link)`
     color: #fff;
+    font-style: italic;
 `;
 
 const MenuBars = styled.i``
