295b328447e1145b49df4717ad4fd224015d6fec
thuotracy <tracyjacobs775@gmail.com>
2023-03-27 14:12:39 +0300

    made chnages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Nav = styled.nav`
     height: 60px;
     background: red;
-`
+`;
+
+const Logo = styled(Link)`
+    color: #fff;
+`;
+
+const MenuBars = styled.i``
+const NavMenu = styled.div``
 
 const Navbar = () => {
   return (
     <Nav>
+        <Logo>LOGO</Logo>
+        <MenuBars />
+        <NavMenu />
         <h1>navbar</h1>
     </Nav>
   )
