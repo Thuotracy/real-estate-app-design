bc1b6960f652d1ed06c00e4537d60d37406b75cc
thuotracy <tracyjacobs775@gmail.com>
2023-03-28 13:39:14 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
 import Navbar from "./components/Navbar";
+import GlobalStyle from "./globalStyles";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Navbar />
     </>
   );
