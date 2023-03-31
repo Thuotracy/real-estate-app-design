2c2332edf667fd6fc9206d8404426f41613af129
thuotracy <tracyjacobs775@gmail.com>
2023-03-31 12:21:44 +0300

    started the homepage

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+// import image
+import Image from '../assets/img/house-banner.png';
+
+// import components
+import Search from '../components/Search';
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return <section>
+    <div>
+      <div>
+        <h1>
+          <span>Rent</span> Your Dream House With Us.
+        </h1>
+        <p>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing 
+          and typesetting industry. Lorem Ipsum has been the industry's standard 
+          dummy text ever since the 1500s, when an unknown printer took a galley 
+          of type and scrambled it to make a type specimen book.
+        </p>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Banner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+// import components
+import Banner from '../components/Banner';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='min-h-[1800px]'>
+      <Banner />
+    </div>);
 };
 
 export default Home;
