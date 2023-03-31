723b5afde644bbb1632c2c3ce6700af4db86e1dd
thuotracy <tracyjacobs775@gmail.com>
2023-03-31 12:02:39 +0300

    added the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+// import link
+import { Link } from 'react-router-dom';
+
+// import logo
+import Logo from '../assets/img/logo.svg';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container mx-auto'>
+        {/* logo */}
+        <Link to='/'>
+          <img src={Logo} alt=''/>
+        </Link>
+      </div>
+    </header>);
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// import router
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
