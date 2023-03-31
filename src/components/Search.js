21f312580a05db5bd7e9cef1797ddc1aabd38ce2
thuotracy <tracyjacobs775@gmail.com>
2023-03-31 14:17:52 +0300

    created a search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+// import components
+import CountryDropdown from './CountryDropdown';
+import PropertyDropdown from './PropertyDropdown';
+import PriceRangeDropdown from './PrinceRangeDropdown';
+
+// import icons
+import { RiSearch2Line } from 'react-icons/ri';
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+      <CountryDropdown />
+      <PropertyDropdown />
+      <PriceRangeDropdown />
+      <button>
+        <RiSearch2Line />
+      </button>
+    </div>
+  );
 };
 
 export default Search;
