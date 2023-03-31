542f8ec461450eebb0f5601a1eafee0a3f9abe6a
thuotracy <tracyjacobs775@gmail.com>
2023-03-31 12:09:55 +0300

    created the login and sign buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,18 @@ import Logo from '../assets/img/logo.svg';
 
 const Header = () => {
   return (
-    <header>
-      <div className='container mx-auto'>
+    <header className='py-6 mb-12 border-b'>
+      <div className='container mx-auto flex justify-between items-center'>
         {/* logo */}
         <Link to='/'>
           <img src={Logo} alt=''/>
         </Link>
+
+        {/* button */}
+        <div className='flex items-center gap-6'>
+          <Link className='hover:text-violet-900 transition' to=''>Log In</Link>
+          <Link className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg' to=''>Sign Up</Link>
+        </div>
       </div>
     </header>);
 };
