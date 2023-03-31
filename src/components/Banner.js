163d465edba7f3c270fb38a77db4bbee048acf1c
thuotracy <tracyjacobs775@gmail.com>
2023-03-31 14:32:19 +0300

    styled the search button

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,8 @@ const Banner = () => {
   return (
     <section className="h-full max-h-[640px] mb-8 xl:mb-24">
       <div className="flex flex-col lg:flex-row">
-        <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
+        <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start 
+        text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
           <h1 className="text-4x1 lg:text-[58px] font-semibold leading-none mb-6">
             <span className="text-violet-700">Rent</span> Your Dream House With Us.
           </h1>
