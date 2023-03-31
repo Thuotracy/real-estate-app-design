fa17eeec21b05434e459e6314fb36734016bb25d
thuotracy <tracyjacobs775@gmail.com>
2023-03-31 16:08:57 +0300

    started on the countrydropdown section

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 // import icons
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
+
+// import headless ui
+import { Menu } from '@headlessui/react'
 
 const CountryDropdown = () => {
   return <div>CountryDropdown</div>; 
