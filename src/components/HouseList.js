09d0737792bdd273071da2dae5064093abaf2b9c
thuotracy <tracyjacobs775@gmail.com>
2023-04-04 17:08:23 +0300

    made chnages

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -7,15 +7,26 @@ import { HouseContext } from './HouseContext';
 import House from './House';
 
 // import link
-import { link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // import icons
 import { ImSpinner2 } from 'react-icons/im';
 
 const HouseList = () => {
   const { houses, loading } = useContext(HouseContext);
-  console.log(houses);
-  return <div>Hello</div>;
+  return <section className='mb-20'>
+    <div className='container mx-auto'>
+      <div>
+        {houses.map((house, index) => {
+          return(
+            <Link to={`/property/${house.id}`} key={index}>
+              <House house={house}/>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  </section>
 };
 
 export default HouseList;
