74b33587b899c76803e31726768e19bf85fa6d7a
thuotracy <tracyjacobs775@gmail.com>
2023-04-04 16:42:58 +0300

    started on the houselist context

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+// import context
+import { HouseContext } from './HouseContext';
+
+// import components
+import House from './House';
+
+// import link
+import { link } from 'react-router-dom';
+
+// import icons
+import { ImSpinner2 } from 'react-icons/im';
 
 const HouseList = () => {
+  const {} = useContext(HouseContext);
   return <div>HouseList</div>;
 };
 
