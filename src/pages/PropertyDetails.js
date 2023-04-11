b9f552030d78e5c8cb085d28f07d02e17798741b
thuotracy <tracyjacobs775@gmail.com>
2023-04-11 15:29:07 +0300

    started the property details page

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+// import data
+import { housesData } from '../data';
+
+// import use params
+import { useParams } from 'react-router-dom';
+
+// import icons
+import { BiBed, BiBath, BiArea } from 'react-icons/bi';
+
 const PropertyDetails = () => {
   return <div>PropertyDetails</div>;
 };
