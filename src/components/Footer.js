0e2c9e98bece0fff929523aa51c17616323139bf
thuotracy <tracyjacobs775@gmail.com>
2023-04-11 16:55:11 +0300

    created the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-black py-8 text-center text-white'>
+      <div className='container mx-auto'>
+        Copyright &copy; 2023 | All Rights Reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
