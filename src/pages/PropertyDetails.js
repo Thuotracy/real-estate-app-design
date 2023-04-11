d0be3de49d43cf8e9dfd814eff866548545ff7b8
thuotracy <tracyjacobs775@gmail.com>
2023-04-11 15:52:21 +0300

    styled the property details

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -23,20 +23,22 @@ const PropertyDetails = () => {
   return <section>
     <div className='container mx-auto min-h-[800px] mb-14'>
 
-      <div className='flex'>
+      <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between'>
         <div>
-          <h2>{house.name}</h2>
-          <h3>{house.address}</h3>
+          <h2 className='text-2x1 font-semibold'>{house.name}</h2>
+          <h3 className='text-lg mb-4'>{house.address}</h3>
         </div>
 
-        <div>
-          <div>{house.type}</div>
-          <div>{house.country}</div>
+        <div className='mb-4 lg:mb-0 flex gap-x-2 text-sm'>
+          <div className='bg-green-500 text-white px-3 rounded-full'>{house.type}</div>
+          <div className='bg-violet-500 text-white px-3 rounded-full'>{house.country}</div>
         </div>
 
-        <div>$ {house.price}</div>
+        <div className='text-3x1 font-semibold text-violet-500'>$ {house.price}</div>
       </div>
 
+      <div></div>
+      
     </div>
   </section>;
 };
